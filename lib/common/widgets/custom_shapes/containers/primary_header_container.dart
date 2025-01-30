34637cf0ca7db553bbea0b787b2699372e366d4f
Wladimir <wladimirss14@gmail.com>
2025-01-30 16:53:18 -0400

    Add: Custom Shape note: flutter upgrade

---
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:flutter_ecommerce_app/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:flutter_ecommerce_app/utils/constants/colors.dart';
+
+class TPrimaryHeaderContainer extends StatelessWidget {
+  const TPrimaryHeaderContainer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TCurvedEdgeWidget(
+      child: Container(
+        color: TColors.primaryColor,
+        padding: const EdgeInsets.all(0),
+        child: SizedBox(
+          height: 400,
+          child: Stack(
+            children: [
+              Positioned(
+                  top: -150,
+                  right: -250,
+                  child: TCircularContainer(
+                    backgroundColor: TColors.white.withOpacity(.1),
+                  )),
+              Positioned(
+                  top: 100,
+                  right: -300,
+                  child: TCircularContainer(
+                    backgroundColor: TColors.white.withOpacity(.1),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/home.dart
+++ lib/features/shop/screens/home.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/common/widgets/custom_shapes/containers/primary_header_container.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
