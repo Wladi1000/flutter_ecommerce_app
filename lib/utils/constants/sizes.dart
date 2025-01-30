bf6d098270f9f6cded3f3b661e86a509fac8a0e7
Wladimir <wladimirss14@gmail.com>
2025-01-30 11:25:27 -0400

    Update: constants - Sizes

--- lib/utils/constants/sizes.dart
+++ lib/utils/constants/sizes.dart
@@ -1,3 +1,4 @@
+
 class TSizes {
   // Padding and margin sizes
   static const double xs = 4.0;
@@ -13,16 +14,15 @@ class TSizes {
   static const double iconLg = 32.0;
 
   // Font sizes
-  static const double fontXs = 12.0;
-  static const double fontSm = 14.0;
-  static const double fontMd = 16.0;
-  static const double fontLg = 18.0;
+  static const double fontSizeSm = 14.0;
+  static const double fontSizeMd = 16.0;
+  static const double fontSizeLg = 18.0;
 
   // Button sizes
   static const double buttonHeight = 18.0;
-  static const double buttonWidth = 120.0;
   static const double buttonRadius = 12.0;
-  static const double elevation = 4.0;
+  static const double buttonWidth = 120.0;
+  static const double buttonElevation = 4.0;
 
   // AppBar height
   static const double appBarHeight = 56.0;
@@ -33,7 +33,7 @@ class TSizes {
   // Default spacing between sections
   static const double defaultSpace = 24.0;
   static const double spaceBtwItems = 16.0;
-  static const double spaceBtwItemsSm = 32.0;
+  static const double spaceBtwSections = 32.0;
 
   // Border radius
   static const double borderRadiusSm = 4.0;
@@ -46,20 +46,20 @@ class TSizes {
   // Product item dimensions
   static const double productImageSize = 120.0;
   static const double productImageRadius = 16.0;
-  static const double productImageHeight = 160.0;
+  static const double productItemHeight = 160.0;
 
-  // input field
+  // Input field
   static const double inputFieldRadius = 12.0;
   static const double spaceBtwInputFields = 16.0;
 
-  // Card sized
+  // Card sizes
   static const double cardRadiusLg = 16.0;
   static const double cardRadiusMd = 12.0;
   static const double cardRadiusSm = 10.0;
   static const double cardRadiusXs = 6.0;
   static const double cardElevation = 2.0;
 
-  // Image carouse height
+  // Image carousel height
   static const double imageCarouselHeight = 200.0;
 
   // Loading indicator size
