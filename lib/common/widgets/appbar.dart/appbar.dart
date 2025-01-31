a2990574c7068875704d236d9b1cb733fc4c8c59
Wladimir <wladimirss14@gmail.com>
2025-01-31 11:21:19 -0400

    Add: appbar widget

---
+++ lib/common/widgets/appbar.dart/appbar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/utils/constants/sizes.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double appBarHeight; // usa la funcion TDeciviceUtility.getAppBarHeight
+
+  const TAppBar(
+      {super.key,
+      required this.appBarHeight,
+      this.title,
+      this.showBackArrow = true,
+      this.leadingIcon,
+      this.actions,
+      this.leadingOnPressed});
+
+  final Widget? title;
+  final bool showBackArrow;
+  final IconData? leadingIcon;
+  final List<Widget>? actions;
+  final VoidCallback? leadingOnPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: TSizes.md),
+      child: AppBar(
+        automaticallyImplyLeading: false,
+        leading: showBackArrow
+            ? IconButton(onPressed: () => Navigator.of(context).pop(), icon: const Icon(Iconsax.arrow_left))
+            : leadingIcon != null ?  IconButton(onPressed: leadingOnPressed, icon: Icon(leadingIcon)) : null,
+            title: title,
+            actions: actions,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(appBarHeight);
+}
