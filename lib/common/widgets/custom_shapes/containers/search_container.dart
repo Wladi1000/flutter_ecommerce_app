800234f9833ab13e0bdb3bd00d9ee0fe3d8a2409
Wladimir <wladimirss14@gmail.com>
2025-01-31 15:47:26 -0400

    Update: prepare future function on seatch container

--- lib/common/widgets/custom_shapes/containers/search_container.dart
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -11,33 +11,38 @@ class TSearchContainer extends StatelessWidget{
     required this.text, 
     this.icon = Iconsax.search_normal, 
     this.shadowBackground = true, 
-    this.showBorder = true
+    this.showBorder = true, 
+    this.onTap
   });
 
   final String text;
   final IconData? icon;
   final bool shadowBackground, showBorder;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
-      child: Container(
-        width: TDeviceUtility.getScreenWidth(context),
-        padding: const EdgeInsets.all(TSizes.md),
-        decoration: BoxDecoration(
-          color: shadowBackground? dark? TColors.dark : TColors.light : Colors.transparent,
-          borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
-          border: showBorder ? Border.all(color: TColors.grey) : null,
-        ),
-        child: Row(
-          children: [
-            Icon(icon, color: TColors.darkerGrey,),
-            const SizedBox(width: TSizes.spaceBtwItems,),
-            Text(text, style: Theme.of(context).textTheme.bodySmall),
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+        child: Container(
+          width: TDeviceUtility.getScreenWidth(context),
+          padding: const EdgeInsets.all(TSizes.md),
+          decoration: BoxDecoration(
+            color: shadowBackground? dark? TColors.dark : TColors.light : Colors.transparent,
+            borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+            border: showBorder ? Border.all(color: TColors.grey) : null,
+          ),
+          child: Row(
+            children: [
+              Icon(icon, color: TColors.darkerGrey,),
+              const SizedBox(width: TSizes.spaceBtwItems,),
+              Text(text, style: Theme.of(context).textTheme.bodySmall),
+            ],
+          ),
         ),
       ),
     );
