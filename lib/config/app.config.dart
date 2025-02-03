301bc49fe932558ab696dbf2fa612c0fecef4dae
Wladimir <wladimirss14@gmail.com>
2025-02-03 11:42:28 -0400

    Add: Old Version -> config

---
+++ lib/config/app.config.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+const APP_NAME = 'Guayana App';
+const APP_VERSION = '0.1.0';
+const APP_DEBUG = false;
+
+const API_URL = '';
+const ENVIRONMENT = 'development';
