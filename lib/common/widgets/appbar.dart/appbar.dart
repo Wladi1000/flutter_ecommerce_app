9269c27dbb29437ff895b6e7627593910877c5b1
Wladimir <wladimirss14@gmail.com>
2025-02-04 15:53:52 -0400

    Fix: agan TAppbar

--- lib/common/widgets/appbar.dart/appbar.dart
+++ lib/common/widgets/appbar.dart/appbar.dart
@@ -9,7 +9,7 @@ class TAppBar extends StatelessWidget implements PreferredSizeWidget {
       {super.key,
       this.appBarHeight = TSizes.appBarHeight,
       this.title,
-      this.showBackArrow = true,
+      this.showBackArrow = false,
       this.leadingIcon,
       this.actions,
       this.leadingOnPressed});
