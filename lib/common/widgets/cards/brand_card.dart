918850ce1b9374e17c41b6ee65887e3e846a0764
Wladimir <wladimirss14@gmail.com>
2025-02-04 10:42:51 -0400

    Refactore: brand card

---
+++ lib/common/widgets/cards/brand_card.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:flutter_ecommerce_app/common/widgets/images/circular_image.dart';
+import 'package:flutter_ecommerce_app/common/widgets/texts/brand_title_with_verified_icon.dart';
+import 'package:flutter_ecommerce_app/features/shop/models/brand.dart';
+import 'package:flutter_ecommerce_app/utils/constants/colors.dart';
+import 'package:flutter_ecommerce_app/utils/constants/enums.dart';
+import 'package:flutter_ecommerce_app/utils/constants/sizes.dart';
+import 'package:flutter_ecommerce_app/utils/helpers/helper_functions.dart';
+
+class TBrandCard extends StatelessWidget {
+  /// Default constructor for the TBrandCard.
+  ///
+  /// Parameters:
+  ///   - brand: The brand model to display.
+  ///   - showBorder: A flag indicating whether to show a border around the card.
+  ///   - onTap: Callback function when the card is tapped.
+  const TBrandCard({
+    super.key,
+    required this.brand,
+    required this.showBorder,
+    this.isNetworkImage = false,
+    this.onTap,
+  });
+
+  final BrandModel brand;
+  final bool showBorder;
+  final bool isNetworkImage;
+  final void Function()? onTap;
+
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = THelperFunctions.isDarkMode(context);
+
+    return GestureDetector(
+      onTap: onTap,
+      /// Container Design
+      child: TRoundedContainer(
+        showBorder: showBorder,
+        backgroundColor: Colors.transparent,
+        padding: const EdgeInsets.all(TSizes.sm),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            /// -- Icon
+            Flexible(
+              child: TCircularImage(
+                image: brand.image,
+                isNetworkImage: isNetworkImage,
+                backgroundColor: Colors.transparent,
+                overlayColor: isDark ? TColors.white : TColors.black,
+              ),
+            ),
+            const SizedBox(width: TSizes.spaceBtwItems / 2),
+
+            /// -- Texts
+            // [Expanded] & Column [MainAxisSize.min] is important to keep the elements in the vertical center and also
+            // to keep text inside the boundaries.
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TBrandTitleWithVerifiedIcon(title: brand.name, brandTextSize: TextSizes.large),
+                  Text(
+                    '${brand.productsCount ?? 0} products',
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_app/common/widgets/appbar.dart/appbar.dart';
-import 'package:flutter_ecommerce_app/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:flutter_ecommerce_app/common/widgets/cards/brand_card.dart';
 import 'package:flutter_ecommerce_app/common/widgets/custom_shapes/containers/search_container.dart';
-import 'package:flutter_ecommerce_app/common/widgets/images/circular_image.dart';
 import 'package:flutter_ecommerce_app/common/widgets/layouts/grid_layout.dart';
 import 'package:flutter_ecommerce_app/common/widgets/products/cart/cart_menu_icon.dart';
-import 'package:flutter_ecommerce_app/common/widgets/texts/brand_title_with_verified_icon.dart';
 import 'package:flutter_ecommerce_app/common/widgets/texts/section_heading.dart';
+import 'package:flutter_ecommerce_app/features/shop/models/brand.dart';
 import 'package:flutter_ecommerce_app/utils/constants/colors.dart';
-import 'package:flutter_ecommerce_app/utils/constants/enums.dart';
 import 'package:flutter_ecommerce_app/utils/constants/image_strings.dart';
 import 'package:flutter_ecommerce_app/utils/constants/sizes.dart';
 import 'package:flutter_ecommerce_app/utils/device/device_utility.dart';
@@ -19,7 +17,7 @@ class StorePage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    final darkMode = THelperFunctions.isDarkMode(context);
+    // final darkMode = THelperFunctions.isDarkMode(context);
     return Scaffold(
       appBar: TAppBar(
         showBackArrow: false,
@@ -53,43 +51,9 @@ class StorePage extends StatelessWidget{
                   const SizedBox(height: TSizes.spaceBtwItems / 1.5,),
 
                   TGridLayout(itemCount: 4, mainAxisExtent: 80, itemBuilder: (_, index) {
-                    return GestureDetector(
-                      onTap: (){},
-                      child: TRoundedContainer(
-                        padding: const EdgeInsets.all(TSizes.sm),
-                        showBorder: true,
-                        backgroundColor: Colors.transparent,
-                        child: Row(
-                          children: [
-                            // Icon
-                            Flexible(
-                              child: TCircularImage(
-                                isNetworkImage: false,
-                                image: TImages.clothIcon, 
-                                backgroundColor: Colors.transparent,
-                                overlayColor: darkMode?TColors.white : TColors.black,
-                              ),
-                            ),
-                            const SizedBox(height: TSizes.spaceBtwItems / 2,),
-                      
-                            // Text
-                            Expanded(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const TBrandTitleWithVerifiedIcon(title: 'Nike', brandTextSize: TextSizes.large,),
-                                  Text(
-                                    '256 products',
-                                    overflow: TextOverflow.ellipsis,
-                                    style: Theme.of(context).textTheme.labelMedium
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
+                    return TBrandCard(
+                      showBorder: true,
+                      brand: BrandModel(image: TImages.nikeLogo, name: 'Nike', productsCount: 12,),
                     );
                   }),
                 ],
