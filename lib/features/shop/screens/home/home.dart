5e3a6191ac2e8093f43bfc52a2f7e803b6bd8286
Wladimir <wladimirss14@gmail.com>
2025-02-04 17:07:43 -0400

    fix: dart fix --apply

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -45,7 +45,7 @@ class HomeScreen extends StatelessWidget {
                       ],
                     ),
                   ),
-                  const SizedBox(height: TSizes.spaceBtwSections,)
+                  SizedBox(height: TSizes.spaceBtwSections,)
                 ],
               ),
             ),
--- lib/features/shop/screens/wishlist/wishlist.dart
+++ lib/features/shop/screens/wishlist/wishlist.dart
@@ -20,7 +20,7 @@ class FavoriteScreen extends StatelessWidget{
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(TSizes.defaultSpace),
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
             children: [
               TGridLayout(itemCount: 6, itemBuilder: (_, index) => const TProductCardVertical())
